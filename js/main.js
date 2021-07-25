b91e090a31ef9efd61e083d51c12bbd972f584dc
Kelvin Elliott <kelliott11@yahoo.com>
2021-07-25 19:00:16 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+import { clearSearchText, setSearchFocus, showClearTextButton, clearPushListener } from './searchBar.js';
+import { getSearchTerm, retrieveSearchResults } from './dataFunctions.js';
+import {
+  deleteSearchResults,
+  buildSearchResults,
+  clearStatsLine,
+  setStatsLine,
+} from './searchResults.js';
+
+document.addEventListener('readystatechange', (event) => {
+  if (event.target.readyState === 'complete') {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  setSearchFocus();
+
+  //  TODO: 3 listeners clear text
+  const search = document.getElementById('search');
+  search.addEventListener('input', showClearTextButton);
+  const clear = document.getElementById("clear");
+  clear.addEventListener("click", clearSearchText);
+  clear.addEventListener("keydown", clearPushListener)
+  const form = document.getElementById('searchBar');
+  form.addEventListener('submit', submitTheSearch);
+};
+
+// Procedural "workflow" function
+const submitTheSearch = (event) => {
+  event.preventDefault();
+  deleteSearchResults();
+  processTheSearch();
+  setSearchFocus();
+};
+
+// Procedural
+const processTheSearch = async () => {
+  clearStatsLine();
+  const searchTerm = getSearchTerm();
+  if (searchTerm === '') return;
+  const resultArray = await retrieveSearchResults(searchTerm);
+  if (resultArray.length) buildSearchResults(resultArray);
+  setStatsLine(resultArray.length);
+};
